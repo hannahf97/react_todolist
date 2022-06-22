b67088e560af601f05147f24ea82e4d40a13f1f3
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-06-22 10:50:18 +0900

    [Add] first commit

---
+++ todoApp/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>이거 되냐</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
